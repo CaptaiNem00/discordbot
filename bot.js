cad8d854238a2cd6b391732479093b0f949d24a6
CaptaiNem00 <48620000+CaptaiNem00@users.noreply.github.com>
2020-09-02 16:50:42 +0300

    a

--- bot.js
+++ bot.js
@@ -46,6 +46,9 @@ client.on('message', msg => {
   if (msg.content.toLowerCase() === 'ahmet ifşa 2') {
     msg.channel.send('hay allah elimden kaçtı',{files:["./images/ahmet ifşa 2.jpg"]});
   }
+  if (msg.content.toLowerCase() === 'ahmet ifşa 3') {
+    msg.channel.send('hay allah elimden kaçtı',{files:["./images/ahmet ifşa 3.jpg"]});
+  }
   if (msg.content.toLowerCase() === 'help') {
     msg.channel.send(commandlist);
   }
