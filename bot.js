3c70a98156a4a1caa1f28fc973c73201817f802f
CaptaiNem00 <48620000+CaptaiNem00@users.noreply.github.com>
2020-09-02 16:57:13 +0300

    a

--- bot.js
+++ bot.js
@@ -653,6 +653,26 @@ const args = message.content.split(" ");
     return message.channel.send(err);
   } 
 }
+if (message.content ==='yarrak')
+{
+const args = message.content.split(" ");
+  try {
+    var connection = await voiceChannel.join();
+    execute(message)
+    voiceChannel.join().then(connection =>
+    {
+       const dispatcher = connection.play("./sounds/yarrak.mp3");
+       dispatcher.setVolumeLogarithmic(5/ 5);
+       dispatcher.on("finish", () => {
+         voiceChannel.leave();
+         });
+     }).catch(err => console.log(err));     
+    }  
+    catch (err) {
+    console.log(err);
+    return message.channel.send(err);
+  } 
+}
 });
 
 
